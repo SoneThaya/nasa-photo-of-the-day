66771eb65788e9d03caf569ca48a12b7be20a04c
Sone <sthaya@gmail.com>
2020-04-15 11:30:45 -0700

    started project, fetched data using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
+
+const baseURL = 'https://api.nasa.gov'
+const api_key = 'kfsAd7oZSGgkX8uXhUr3RI6s8QoO27sEFTpxWZus'
+
+
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(`${baseURL}/planetary/apod?api_key=DEMO_KEY`)
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <div className="App">
       <p>
